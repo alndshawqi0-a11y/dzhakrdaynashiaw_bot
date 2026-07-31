6ea939501bc44d35a7d29c6bb5d257a872ee8a7d
alndshawqi0-a11y <alndshawqi0@gmail.com>
2026-07-31 23:42:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,21 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 
-TOKEN = "8644286932:AAE-bJGIwynojZjOYynK6J6Qy-Zm06W8Wbg"
+TOKEN = os.getenv("BOT_TOKEN") or "8644286932:AAE-bJGIwynojZjOyYnK6J6Qy-Zm06W8wbg"
 
 async def delete_stickers(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message and update.message.sticker:
         try:
             await update.message.delete()
-            print("ستیکەرێک سڕایەوە!")
         except Exception as e:
-            print(f"هەڵە لە سڕینەوەی ستیکەردا: {e}")
+            pass
 
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
+    # filters.Sticker گشتییە و هەموو جۆرە ستیکەرێکی وێنەیی، جووڵاو و ڤیدیۆیی دەگرێتەوە
     app.add_handler(MessageHandler(filters.Sticker.ALL, delete_stickers))
-    print("بوتەکە کارا بوو و ئێستا چاودێری ستیکەرەکان دەکات...")
     app.run_polling()
 
-if __name__ == "__main__":
+if name == "main":
     main()
